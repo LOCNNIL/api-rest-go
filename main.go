5f08fea506cf46a73a6fbdada1921c1023c98371
Lincoln Wallace <lincolnble@hotmail.com>
2023-04-08 15:03:01 -0300

    add: Create database connection with GORM
    Signed-off-by: Lincoln Wallace <lincolnble@hotmail.com>

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 
+	"github.com/LOCNNIL/api-rest-go/database"
 	"github.com/LOCNNIL/api-rest-go/models"
 	"github.com/LOCNNIL/api-rest-go/routes"
 )
 
 func main() {
 	models.Personalities = []models.Personality{
-		{Name: "1º Name", History: "First History"},
-		{Name: "2º Name", History: "Secound History"},
-		{Name: "3º Name", History: "Third History"},
+		{Id: 1, Name: "1º Name", History: "First History"},
+		{Id: 2, Name: "2º Name", History: "Secound History"},
+		{Id: 3, Name: "3º Name", History: "Third History"},
 	}
+
+	database.ConectWithDatabase()
 	fmt.Println("Starting the REST server with GO!")
 	routes.HandleRequest()
 }
